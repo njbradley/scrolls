0318699e60bc3bc8b70703edba2749f2f57b8bc6
njbradley <nicholasjohnstonbradley@gmail.com>
2022-06-22 17:03:16 -0700

    moved hitcubes to physics

---
+++ base/physics.cc
@@ -0,0 +1,54 @@
+#include "physics.h"
+
+
+bool IHitCube::contains(ivec3 point) const {
+	return point.x >= position.x and point.y >= position.y and point.z >= position.z
+			and point.x <= position.x + scale and point.y <= position.y + scale and point.z <= position.z + scale;
+}
+
+bool IHitCube::contains(const IHitCube& other) const {
+	return other.position.x >= position.x and other.position.y >= position.y and other.position.z >= position.z
+			and other.position.x + other.scale <= position.x + scale and other.position.y + other.scale <= position.y + scale
+			and other.position.z + other.scale <= position.z + scale;
+}
+
+bool IHitCube::collides(ivec3 point) const {
+	return point.x > position.x and point.y > position.y and point.z > position.z
+			and point.x < position.x + scale and point.y < position.y + scale and point.z < position.z + scale;
+}
+
+bool IHitCube::collides(const IHitCube& other) const {
+	return ((position.x <= other.position.x and position.x + scale > other.position.x)
+			or (other.position.x <= position.x and other.position.x + other.scale > position.x))
+			and ((position.y <= other.position.y and position.y + scale > other.position.y)
+			or (other.position.y <= position.y and other.position.y + other.scale > position.y))
+			and ((position.z <= other.position.z and position.z + scale > other.position.z)
+			or (other.position.z <= position.z and other.position.z + other.scale > position.z));
+}
+
+
+
+HitCube HitCube::transform_in(const HitCube& cube) const {
+	return HitCube(transform_in(cube.position), cube.scale, glm::inverse(rotation) * cube.rotation);
+}
+
+HitCube HitCube::transform_out(const HitCube& cube) const {
+	return HitCube(transform_out(cube.position), cube.scale, rotation * cube.rotation);
+}
+
+bool HitCube::contains(vec3 point) const {
+	point = transform_in(point);
+	return point.x >= position.x and point.y >= position.y and point.z >= position.z
+				and point.x <= position.x + scale and point.y <= position.y + scale and point.z <= position.z + scale;
+}
+
+bool HitCube::collides(vec3 point) const {
+	point = transform_in(point);
+	return point.x > position.x and point.y > position.y and point.z > position.z
+			and point.x < position.x + scale and point.y < position.y + scale and point.z < position.z + scale;
+}
+
+bool HitCube::collides(const HitCube& globalother) const {
+	HitCube other = transform_in(globalother);
+	return false;
+}
---
+++ base/physics.h
@@ -0,0 +1,135 @@
+#ifndef BASE_PHYSICS
+#define BASE_PHYSICS
+
+#include "common.h"
+
+
+// Class that represents an axis alligned,
+// integer alligned, cube hitbox.
+struct IHitCube {
+	ivec3 position;
+	int scale;
+	
+	IHitCube();
+	IHitCube(ivec3 pos, int nscale);
+	
+	ivec3 midpoint() const;
+	
+	// A point is contained if it is inside the box or on the edge
+	bool contains(ivec3 point) const;
+	// a box is contained when no part of it extends outside the box
+	// (so the box can also touch the edges)
+	bool contains(const IHitCube& other) const;
+	
+	// A point or box collides when it actually penetrates the box, a point
+	// is not colliding if it is on the edge
+	bool collides(ivec3 point) const;
+	bool collides(const IHitCube& other) const;
+	
+	ivec3 transform_in(ivec3 point) const;
+	vec3 transform_in(vec3 point) const;
+	ivec3 transform_out(ivec3 point) const;
+	vec3 transform_out(vec3 point) const;
+	IHitCube transform_in(const IHitCube& cube) const;
+	IHitCube transform_out(const IHitCube& cube) const;
+	
+	friend IHitCube operator+(const IHitCube& cube, ivec3 pos);
+	friend IHitCube operator+(ivec3 pos, const IHitCube& cube);
+	IHitCube& operator+=(ivec3 pos);
+};
+
+
+struct HitCube {
+	vec3 position;
+	int scale;
+	quat rotation;
+	
+	HitCube();
+	HitCube(const IHitCube& icube);
+	HitCube(vec3 pos, int nscale, quat rot);
+	
+	bool contains(vec3 point) const;
+	bool contains(const HitCube& other) const;
+	
+	bool collides(vec3 point) const;
+	bool collides(const HitCube& other) const;
+	
+	vec3 transform_in(vec3 point) const;
+	vec3 transform_out(vec3 point) const;
+	HitCube transform_in(const HitCube& cube) const;
+	HitCube transform_out(const HitCube& cube) const;
+};
+
+
+
+
+
+
+///// INLINE FUNCTIONS
+
+inline IHitCube::IHitCube() {
+	
+}
+
+inline IHitCube::IHitCube(ivec3 pos, int nscale): position(pos), scale(nscale) {
+	
+}
+
+inline ivec3 IHitCube::midpoint() const {
+	return position + scale/2;
+}
+
+inline ivec3 IHitCube::transform_in(ivec3 point) const {
+	return point - position;
+}
+
+inline vec3 IHitCube::transform_in(vec3 point) const {
+	return point - vec3(position);
+}
+
+inline ivec3 IHitCube::transform_out(ivec3 point) const {
+	return point + position;
+}
+
+inline vec3 IHitCube::transform_out(vec3 point) const {
+	return point + vec3(position);
+}
+
+
+
+inline IHitCube operator+(const IHitCube& cube, ivec3 pos) {
+	return IHitCube(cube.position + pos * cube.scale, cube.scale);
+}
+
+inline IHitCube operator+(ivec3 pos, const IHitCube& cube) {
+	return IHitCube(cube.position + pos * cube.scale, cube.scale);
+}
+
+inline IHitCube& IHitCube::operator+=(ivec3 pos) {
+	position += pos * scale;
+	return *this;
+}
+
+
+
+inline HitCube::HitCube() {
+	
+}
+
+inline HitCube::HitCube(const IHitCube& icube): position(icube.position), scale(icube.scale), rotation(1,0,0,0) {
+	
+}
+
+inline HitCube::HitCube(vec3 pos, int nscale, quat rot): position(pos), scale(nscale), rotation(rot) {
+	
+}
+
+inline vec3 HitCube::transform_in(vec3 point) const {
+	return glm::inverse(rotation) * (point - position);
+}
+
+inline vec3 HitCube::transform_out(vec3 point) const {
+	return rotation * point + position;
+}
+
+#endif
